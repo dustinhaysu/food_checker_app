e2ed1334d774b3c72a31365a173e4b56faa1dd95
Dustin Hays <hays.dustin.u@gmail.com>
2023-01-31 05:54:19 -0800

    VOD complete

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ if(inputVal.length !== 12){
         if(data.status === 1){
           const item = new ProductInfo(data.product);
            item.showInfo();
+           item.listIngredients()
         } else if (data.status === 0) {//use JSON data
           alert(`product ${inputVal} not found. Please try another.`)
         }
@@ -40,5 +41,35 @@ class ProductInfo {
 
   showInfo() {
     document.getElementById('product-img').src= this.image
-    document.getElementById('product-name').innerText = this.name  }
+    document.getElementById('product-name').innerText = this.name  
+  }
+
+listIngredients () {
+  let tableRef = document.getElementById('ingredient-table')
+
+  for(let i = 1; i<tableRef.length;) {//in this case i++ is removed because each iteration of i makes the table smaller by one row. by allowing i to increase by 1, rows of the table would be skipped and left intact.
+    //we need to keep first row 
+    tableRef.deleteRow(i);
+  }
+
+  if(!(this.ingredients == null)){ // loose not strict ==, so will include 0 undefined etc. 
+  for(let key in this.ingredients){
+    let newRow = tableRef.insertRow(-1)
+    let newICell = newRow.insertCell(0)
+    let newVCell = newRow.insertCell(1)
+    let newIText = document.createTextNode(
+      this.ingredients[key].text
+    )
+    let vegStatus = this.ingredients[key].vegetarian == null ? 'unknown' : this.ingredients[key].vegetarian // ternary to redefine undefined
+    let newVText = document.createTextNode(vegStatus)
+    newICell.appendChild(newIText)
+    newVCell.appendChild(newVText)
+    if (vegStatus === 'no'){
+      newVCell.classList.add('non-veg-item')//adds a class to element and turn item red in CSS
+    } else if (vegStatus === 'unknown' || vegStatus === 'maybe') {
+      newVCell.classList.add('unknown-maybe-item')// turn item yellow
+    }
+  }
+  }
+}
 }
